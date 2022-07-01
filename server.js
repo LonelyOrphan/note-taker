e5ff5652b64847d70e8bbc5954278e2868ddb581
Natasha Beddow <beddow90@gmail.com>
2022-07-01 14:45:55 +0100

    added port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App is listening at http://localhost:${port}`);
 });
 
