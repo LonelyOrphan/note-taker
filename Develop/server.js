cfd243eb8b59127fe3a3031ab31ededae796b5d0
Natasha Beddow <beddow90@gmail.com>
2022-07-01 13:50:39 +0100

    added get routes and path module

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App is listening at http://localhost:${port}`);
+});
+
+// GET / should return homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// GET /notes` should return the `notes.html` file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
