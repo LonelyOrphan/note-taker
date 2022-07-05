b8061641f01ec459d86fceaa1dcb14f9069a2520
Natasha Beddow <beddow90@gmail.com>
2022-07-05 14:36:52 +0100

    added post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const noteData = require("./db/db.json");
 const app = express();
 const port = 3000;
@@ -12,26 +13,32 @@ app.listen(process.env.PORT || port, () => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// HTML Routes
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET /notes` should return the `notes.html` file
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// Get * makes it not work
-// Is it because the index.js is making a get request to a path which is redirected by this?
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+  console.log(noteData);
+  res.json(noteData);
+});
+
 // * `GET *` should return the `index.html` file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// API Routes
-
-// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-app.get("/api/notes", (req, res) => {
-  console.log(noteData);
-  res.sendFile(noteData);
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file,
+// and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved
+// (look into npm packages that could do this for you).
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  noteData.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(noteData), (err) => {
+    if (err) throw err;
+    console.log("Saved!");
+  });
 });
